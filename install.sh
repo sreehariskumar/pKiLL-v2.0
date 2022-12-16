8250dd747a37f7d3fdd138a1cd1514dd629345a3
SREEHARI <68052722+sreehariskumar@users.noreply.github.com>
2022-12-17 01:46:00 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Grant permission
+chmod +x *.sh 
+
+
+#check whether the logged in user is root
+[ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
+
+# Find the package_manager and install the packages
+declare -A osInfo;
+osInfo[/etc/debian_version]="apt-get install -y"
+osInfo[/etc/alpine-release]="apk --update add"
+osInfo[/etc/centos-release]="yum install -y"
+osInfo[/etc/fedora-release]="dnf install -y"
+
+for f in ${!osInfo[@]}
+do
+    if [[ -f $f ]];then
+        package_manager=${osInfo[$f]}
+    fi
+done
+
+package="git python3 python3-tk"
+
+${package_manager} ${package}
+
+# Installation completed
+echo "Installation has completed successfully."
